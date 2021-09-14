d92bc896d8c28e1b9e06337adcb585d9431b6d9a
Nakhunter <90643079+Nakhunter@users.noreply.github.com>
2021-09-14 12:43:57 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,8 +1,8 @@
 const textConfig = {
   text1: "He luu em!",
   text2: "Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Em yêu Anh có phải không nào ._.",
-  text4: "Nếu em ko trả lời mà thoát ra tức là đồng ý làm người yêu anh rùi đó nha :v",
+  text3: "Em thích Anh có phải không nào ._.",
+  text4: "Nếu em ko trả lời mà thoát ra tức là đồng ý rùi đó nha :v",
   text5: "Anh mơ à???",
   text6: "Cũng có <3",
   text7: "Nói lí do em thích anh đi :vvvv",
